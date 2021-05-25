48bc49be02c499596744e06c8dc94ff6225e7d64
xiyu1984 <shanellyy@126.com>
2021-05-25 16:34:02 +0800

    2021.5.25 day

--- httpRequest.py
+++ httpRequest.py
@@ -5,7 +5,9 @@ from hexbytes import HexBytes
 import requests
 import json
 import time
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def getPlatON_Addr():
@@ -74,12 +76,33 @@ def testPD():
 def platonBanlenceCalc():
     w3 = Web3(HTTPProvider("http://192.168.1.49:6789"))
     platon = PlatON(w3)
-    #print(platon.blockNumber)
     cb = platon.getBalance('lat1258ehcq73ux7wqsuurtke9lss9h4prck4ld34x') * 1e-18
     print(cb)
 
+    ccode = platon.getCode('lat1258ehcq73ux7wqsuurtke9lss9h4prck4ld34x')
+    print(len(ccode))
+
+
+def makeAnalisis():
+    df_Addr_Value = pd.read_csv('./PlatON/data/latAVmain 1621868698.csv')
+
+    print(len(df_Addr_Value))
+    accounts = df_Addr_Value[df_Addr_Value['value'] > (1e-6)]['value']
+    accounts = accounts.apply(np.log10)
+    plt.hist(accounts, bins=1000)
+    print(len(accounts))
+
+    spaceL = [-6, 0, 2, np.log10(150), 3, 4, 5, 6, 7, 8, 9, 10]
+    areas = pd.cut(accounts.values, spaceL)
+    value_counts = areas.value_counts()
+    #print(value_counts.index)
+    #print(value_counts.values)
+    print(value_counts)
+
+    plt.show()
 
 #getPlatON_Addr()
 #getPlatON_Addr2()
 #testPD()
-platonBanlenceCalc()
+#platonBanlenceCalc()
+makeAnalisis()
