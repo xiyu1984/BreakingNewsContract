66a628402ea0e0fbd44fce0e268a7a7ec0f4d47b
xiyu1984 <shanellyy@126.com>
2021-08-03 23:01:02 +0800

    modified hpp

--- BreakingNewsContract/BreakingNews.hpp
+++ BreakingNewsContract/BreakingNews.hpp
@@ -6,28 +6,40 @@
 #include <vector>
 #include <map>
 
-//普通消息，可作为观点信息
+//普通消息
 struct Message{
     std::string                         msgUserAddress;         //观点发布者地址
-    std::string                         msgContent;             //观点内容，json格式
-    std::vector<std::string>            msgSupporter;           //观点支持者列表
-    std::vector<std::string>            msgOpponents;           //观点反对者列表
+    std::string                         msgContent;             //观点内容
+    std::vector<std::string>            msgImages;              //图片链接列表
+    std::vector<std::string>            msgSupporter;           //观点点赞者地址列表
+    std::vector<std::string>            msgOpponents;           //观点踩者地址列表
 
-    PLATON_SERIALIZE(Message, (msgUserAddress)(msgContent)(msgSupporter)(msgSupporter))
+    PLATON_SERIALIZE(Message, (msgUserAddress)(msgContent)(msgImages)(msgSupporter)(msgSupporter))
+};
+
+//观点，观点包括支持和反对
+struct Viewpoint : public Message
+{
+    bool            point;          //支持true，反对false
+    PLATON_SERIALIZE_DERIVED(Viewpoint, Message, (point))
 };
 
 //一条爆料
 struct News: public Message
 {
-    std::string                         NewTitle;               //爆料标题，内部会拼接成唯一标识
-    std::vector<Message>                Viewpoints;             //这条爆料后面跟帖的观点
+    std::string                             NewTitle;               //爆料标题
+    platon::u128                            NewID;                  //爆料唯一标识
+    std::vector<Viewpoint>                  Viewpoints;             //这条爆料后面跟帖的观点
 
-    PLATON_SERIALIZE_DERIVED(News, Message, (NewTitle)(Viewpoints))
+    PLATON_SERIALIZE_DERIVED(News, Message, (NewTitle)(NewID)(Viewpoints))
 };
 
 struct UserInfo {
-    std::string                         UserAddress;            //用户地址
-    std::vector<std::string>            UserNews;               //用户发布过的breaking news title
+    std::string                                                     UserAddress;            //用户地址
+    std::vector<std::pair<std::string, platon::u128>>               UserNews;               //用户发布过的breaking news title and ID  
+    int16_t                                                         UserCredibility;        //用户可信度，满分100，初始分数0，最低为-100？
+    
+    PLATON_SERIALIZE(UserInfo, (UserAddress)(UserNews)(UserCredibility))
 };
 
 CONTRACT BreakingNews: public platon::Contract
@@ -36,6 +48,6 @@ CONTRACT BreakingNews: public platon::Contract
 public:
     platon::StorageType<"BreakingNews"_n, std::list<News>>              mBreakingNews;      //存放breaking news
     platon::StorageType<"Users"_n, std::map<std::string, UserInfo>>     mUsers;             //存放用户信息
-    platon::StorageType<"NewsCount"_n, platon::u128>                    mNewsCount;         //当前已发布的news数量，用于拼接生成爆料唯名称唯一标识
+    platon::StorageType<"NewsCount"_n, platon::u128>                    mNewsCount;         //当前已发布的news数量，自增用于生成爆料唯一标识
     platon::StorageType<"hashChain"_n, std::vector<platon::bytes>>      mNewsHashChain;     //爆料哈希链，后续爆料信息会滚动删除，链上仅存哈希链信息用于验证信息是否真实
 };
