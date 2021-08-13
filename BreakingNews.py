3298caa908c00c3b458d159fc7ed3deba75f93fc
xiyu1984 <shanellyy@126.com>
2021-08-13 14:47:09 +0800

    add abi json

--- BreakingNews.py
+++ BreakingNews.py
@@ -6,6 +6,7 @@ from client_sdk_python.utils import contracts
 import json
 
 breakingNewsContractAddr = 'lat17005cansj4xwrdfcxttz003tg0m0drqugwm670'
+#breakingNewsContractAddr = 'lat1xezn8pp6vfaju8f47zh4dcxgt0nmqqhj9x703e'
 clientAccount = 'lat1ar0s6re3qpe3rt39523qw4jars6s4sdhak459n'
 devIP = 'http://47.241.69.26:6789'
 
@@ -21,11 +22,50 @@ def BNtest():
 
     print(hello.functions.getOwner().call())
 
-    tx_receipt = hello.functions.createNews('hello', 'world is changing', ['no image'], '2021.8.13').transact({'from':clientAccount,'gas':1500000})
+    tx_receipt = hello.functions.createNews('hello', 'world is changing', [], '2021.8.13').transact({'from':clientAccount,'gas':1500000})
     receipt_info = platon.waitForTransactionReceipt(tx_receipt)
     print(receipt_info)
 
-    print(hello.functions.getNews().call())
+    #print(hello.functions.getNews().call())
+
+
+def addViewpoint():
+    fp = open('./PlatON/config/BreakingNews.abi.json')
+    breakingNews_abi = json.load(fp)
+
+    w3 = Web3(HTTPProvider(devIP))
+    platon = PlatON(w3)
+    hello = platon.wasmcontract(address=breakingNewsContractAddr, abi=breakingNews_abi, vmtype=1)
+
+    tx_receipt = hello.functions.createViewPoint(1, 'world is not changing', ['no image'], False, '2021.8.13').transact({'from':clientAccount,'gas':1500000})
+    receipt_info = platon.waitForTransactionReceipt(tx_receipt)
+    print(receipt_info)
+
+
+def likeNews():
+    fp = open('./PlatON/config/BreakingNews.abi.json')
+    breakingNews_abi = json.load(fp)
+
+    w3 = Web3(HTTPProvider(devIP))
+    platon = PlatON(w3)
+    hello = platon.wasmcontract(address=breakingNewsContractAddr, abi=breakingNews_abi, vmtype=1)
+
+    tx_receipt = hello.functions.likeNews(1).transact({'from':clientAccount,'gas':1500000})
+    receipt_info = platon.waitForTransactionReceipt(tx_receipt)
+    print(receipt_info)
+
+def dislikeNews():
+    fp = open('./PlatON/config/BreakingNews.abi.json')
+    breakingNews_abi = json.load(fp)
+
+    w3 = Web3(HTTPProvider(devIP))
+    platon = PlatON(w3)
+    hello = platon.wasmcontract(address=breakingNewsContractAddr, abi=breakingNews_abi, vmtype=1)
+
+    tx_receipt = hello.functions.dislikeNews(1).transact({'from':clientAccount,'gas':1500000})
+    receipt_info = platon.waitForTransactionReceipt(tx_receipt)
+    print(receipt_info)
+
 
 def simpleGet():
     fp = open('./PlatON/config/BreakingNews.abi.json')
@@ -38,5 +78,22 @@ def simpleGet():
     print(hello.functions.getNews().call())
 
 
+def clearData():
+    fp = open('./PlatON/config/BreakingNews.abi.json')
+    breakingNews_abi = json.load(fp)
+
+    w3 = Web3(HTTPProvider(devIP))
+    platon = PlatON(w3)
+    hello = platon.wasmcontract(address=breakingNewsContractAddr, abi=breakingNews_abi, vmtype=1)
+
+    tx_receipt = hello.functions.clear().transact({'from':clientAccount,'gas':1500000})
+    receipt_info = platon.waitForTransactionReceipt(tx_receipt)
+    print(receipt_info)
+
+
 #BNtest()
 simpleGet()
+#addViewpoint()
+#likeNews()
+#dislikeNews()
+#clearData()
